3942bb3b54bb193109649291b82fa8751ab39c05
divyap3009 <divyp3009@gmail.com>
2024-10-03 22:41:35 +0530

    remove backend

--- Backend/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { SignUp, Login } from "../controller/UserController.js";
-
-router.post("/signup", SignUp);
-router.post("/login", Login);
-
-export default router;
